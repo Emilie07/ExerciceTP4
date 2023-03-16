71fc7f71c3afea7973fce3b1393e044bf05a2269
manceramontanoe <manceramontanoe@educ.college-montreal.qc.ca>
2023-03-16 16:21:11 -0400

    on_mouse_press doesnt work

--- ExerciceTP4.py
+++ ExerciceTP4.py
@@ -10,15 +10,19 @@ SCREEN_HEIGHT = 600
 COLORS = [arcade.color.AFRICAN_VIOLET, arcade.color.AMARANTH_PINK,
 arcade.color.ARYLIDE_YELLOW, arcade.color.BLEU_DE_FRANCE]
 
-def on_update(self):
-    if cercle_x < rayon_cercle:
-        cercle_x *= -1
-    if cercle_x > SCREEN_WIDTH - rayon_cercle:
-        cercle_x *= -1
-    if cercle_y < rayon_cercle:
-        cercle_y *= -1
-    if cercle_y > SCREEN_HEIGHT - rayon_cercle:
-        cercle_y *= -1
+
+def cercles(self):
+    for _ in range(20)
+
+    def on_update(self):
+        if cercle_x < rayon_cercle:
+            cercle_x *= -1
+        if cercle_x > SCREEN_WIDTH - rayon_cercle:
+            cercle_x *= -1
+        if cercle_y < rayon_cercle:
+            cercle_y *= -1
+        if cercle_y > SCREEN_HEIGHT - rayon_cercle:
+            cercle_y *= -1
 
 def on_draw(self):
    arcade.start_render()
@@ -28,10 +32,6 @@ class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.liste_cercles = []
-
-    def cercles(self):
-        for _ in range(20)
-        ####
 
 def main():
     my_game = MyGame(800, 600)
